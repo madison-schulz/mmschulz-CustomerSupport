79e26536a40840492be10363894870d0a4ade8c4
madison-schulz <madison.schulz@gmail.com>
2024-04-01 18:26:10 -0500

    created Attachment POJO

---
+++ src/main/java/org/example/mmschulzcustomersupport/Attachment.java
@@ -0,0 +1,23 @@
+package org.example.mmschulzcustomersupport;
+
+public class Attachment {
+
+    private String name;
+    private Byte contents;
+
+    public String getName() {
+        return name;
+    }
+
+    public Byte getContents() {
+        return contents;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContents(Byte contents) {
+        this.contents = contents;
+    }
+}
