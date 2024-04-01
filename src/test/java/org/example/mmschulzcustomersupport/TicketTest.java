249ba28061ee3a95f6e964b8a532ef200e0ede13
madison-schulz <madison.schulz@gmail.com>
2024-04-01 18:41:53 -0500

    created TicketTest

---
+++ src/test/java/org/example/mmschulzcustomersupport/TicketTest.java
@@ -0,0 +1,13 @@
+package org.example.mmschulzcustomersupport;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketTest {
+    @Test
+    void checkTicketClassWorks() {
+        var Ticket = new Ticket();
+
+    }
+}
