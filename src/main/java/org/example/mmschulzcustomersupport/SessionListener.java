2d0868791db519f36e808054ee5639926f4ea425
madison-schulz <madison.schulz@gmail.com>
2024-04-16 20:03:01 -0500

    created

---
+++ src/main/java/org/example/mmschulzcustomersupport/SessionListener.java
@@ -0,0 +1,26 @@
+package org.example.mmschulzcustomersupport;
+
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionIdListener;
+import jakarta.servlet.http.HttpSessionListener;
+
+@WebListener
+public class SessionListener implements HttpSessionListener, HttpSessionIdListener {
+    @Override
+    public void sessionIdChanged(HttpSessionEvent se, String oldId) {
+        System.out.println(oldId + " changed to " + se.getSession().getId());
+        SessionListUtil.updateSessionId(se.getSession(), oldId);
+    }
+
+    public void sessionCreated(HttpSessionEvent se) {
+        System.out.println(se.getSession().getId() + "created");
+        SessionListUtil.addSession(se.getSession());
+    }
+
+    public void sessionDestroyed(HttpSessionEvent se) {
+        System.out.println(se.getSession().getId() + "destroyed");
+        SessionListUtil.removeSession(se.getSession());
+    }
+}
